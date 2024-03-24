e27ffabbeaaa5de85a153c1235deae01b676e3c6
Mert <132280028+mk-nlp@users.noreply.github.com>
2024-03-24 14:25:15 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import LandingPage from './pages/landingPage'
+
+function App() {
+  
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/landingPage.tsx
@@ -0,0 +1,12 @@
+import BrandComponent from "@/components/brandComponent";
+
+const LandingPage = () => {
+  return (
+    <div className=" grid justify-items-center bg-gray-900 mt-4">
+      <BrandComponent/>
+      <h1>Welcome to iWeather</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
